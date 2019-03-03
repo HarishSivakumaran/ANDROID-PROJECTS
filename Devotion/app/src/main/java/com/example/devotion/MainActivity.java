630e3430e29d459a91a4cfb88f0bc84703e2473f
HarishSivakumaran <s.harish740@gmail.com>
2019-03-03 16:12:57 +0530

    please1

--- Devotion/app/src/main/java/com/example/devotion/MainActivity.java
+++ Devotion/app/src/main/java/com/example/devotion/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.devotion;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.CountDownTimer;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,18 +13,31 @@ import es.dmoral.toasty.Toasty;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button login;
+   public Button login;
+   ConstraintLayout constraintLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         login=findViewById(R.id.login);
+        constraintLayout=findViewById(R.id.layout_root);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(2000);
+        animationDrawable.start();
     }
+
     public void login_method(View view)
     {
         login.setEnabled(false);
         Toasty.success(this,"LOGIN SUCCESS", Toast.LENGTH_SHORT).show();
         new CountDownTimer(3000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
             @Override
             public void onFinish() {
                 login.setEnabled(true);
